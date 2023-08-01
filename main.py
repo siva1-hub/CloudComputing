556ef55fce1c841091b39c2ee28b84f001616bfc
siva1-hub <140937411+siva1-hub@users.noreply.github.com>
2023-08-01 13:20:27 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+
+import os
+
+import unittest
+
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__) + '/src' )))
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__) + '/tests' )))
+
+#from core import get_answer
+
+from core import main
+
+if name
+*___main__':
+
+f = open('packets/tcp.txt', 'r')
+
+g= open('packets/udp.txt','r')
+
+main(f)
